b8d9161cb97a9c59184adad87fd63dc246427009
Nothing <kadamamit462@gmail.com>
2026-04-02 20:56:55 +0530

    Update Start_Controller.bat

--- Start_Controller.bat
+++ Start_Controller.bat
@@ -3,3 +3,16 @@ title Telegram PC Controller
 cd /d "%~dp0"
 start "" pythonw TelegramController.py
 exit
+
+""
+@echo off
+title Telegram PC Controller
+cd /d "%~dp0"
+
+if exist "TelegramPCController.exe" (
+    start "" "TelegramPCController.exe"
+) else (
+    start "" pythonw TelegramController.py
+)
+exit
+""
